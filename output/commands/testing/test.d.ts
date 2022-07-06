10b62bbde4b24942e62abc90f8ad258859fd8df6
MoonLGH <MoonLMC1212@gmail.com>
2022-07-06 19:04:41 +0000

    removed output

--- output/commands/testing/test.d.ts
+++
@@ -1,4 +0,0 @@
-import { Message } from "@open-wa/wa-automate";
-import { Client } from "../../util/extend/Client";
-export declare function run(client: Client, message: Message): Promise<any>;
-export declare const name = "test";
--- output/commands/testing/test.js
+++
@@ -1,4 +0,0 @@
-export function run(client, message) {
-    return client.clientInstances.sendText(message.chatId, "Pong!");
-}
-export const name = "test";
