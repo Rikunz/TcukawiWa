c8d09e6377fd630eb559c41ffdec4cd6e8ab6e40
FarrelAthaillahSachie <121761137+FarrelAthaillahSachie@users.noreply.github.com>
2023-01-06 15:49:53 +0000

    igdl ??

---
+++ src/util/instagram.ts
@@ -0,0 +1,44 @@
+import axios from "axios"
+const BASE_URL = "https://instasupersave.com/"
+
+// npm : fg-ig
+export async function instagram(url: string){
+    try {
+        const resp = await axios(BASE_URL);
+        const cookie = resp.headers["set-cookie"]; // get cookie from request
+        const session = cookie![0].split(";")[0].replace("XSRF-TOKEN=","").replace("%3D", "")
+        
+        var config = {
+            method: 'post',
+            url: `${BASE_URL}api/convert`,
+            headers: { 
+                'origin': 'https://instasupersave.com', 
+                'referer': 'https://instasupersave.com/pt/', 
+                'sec-fetch-dest': 'empty', 
+                'sec-fetch-mode': 'cors', 
+                'sec-fetch-site': 'same-origin', 
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52', 
+                'x-xsrf-token': session, 
+                'Content-Type': 'application/json', 
+                'Cookie': `XSRF-TOKEN=${session}; instasupersave_session=${session}`
+            },
+            data : {
+                url
+            }
+        };
+
+        //REQUEST
+        let response = await axios(config)
+            let ig = []
+            if(Array.isArray(response.data)){
+                response.data.forEach(post => { ig.push(post.sd === undefined ? post.thumb : post.sd.url)})
+            } else {
+                ig.push(response.data.url[0].url)    
+            }
+        
+            return ig
+            
+    } catch(e){
+        return "ERROR"
+    }
+}
