13e7dba24fb840ab38b8b14bce0e6e01e4253373
MoonLGH <MoonLMC1212@gmail.com>
2023-01-26 15:38:08 +0000

    why the fuck file

--- src/commands/downloader/tiktok.ts
+++ src/commands/downloader/tiktok.ts
@@ -1,6 +1,5 @@
 import {Message} from "@open-wa/wa-automate";
 import {Client} from "../../util/extend/Client";
-import axios from "axios";
 import {tiktok} from "../../util/tiktok.js";
 export async function run(client:Client, message:Message) {
   const {args} = client.parseMessage(message);
@@ -10,25 +9,7 @@ export async function run(client:Client, message:Message) {
   }
 
   client.clientInstances?.sendFileFromUrl(message.chatId, resTiktok.noWM, "tiktok", resTiktok.caption);
-  client.clientInstances?.sendImage(message.chatId, bufferToDataUrl("video/mp4", (await getVideo(resTiktok.noWM))), "tiktok.mp4", resTiktok.caption);
 }
-
-function bufferToDataUrl(mimetype: string, buffer: Buffer): string {
-  return `data:${mimetype};base64,${buffer.toString("base64")}`;
-}
-async function getVideo(url:string) {
-  try {
-    const response = await axios({
-      method: "get",
-      url: url,
-      responseType: "arraybuffer",
-    });
-    return response.data;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
 
 export const name = "tiktok";
 export const description = "download tiktok content";
