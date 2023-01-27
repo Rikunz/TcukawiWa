f13b8f63e1d8df0a014b231f2d6be0139fd206db
MoonLGH <MoonLMC1212@gmail.com>
2023-01-27 15:50:30 +0000

    tojs

--- src/commands/misc/gempa.ts
+++ src/commands/misc/gempa.ts
@@ -1,6 +1,6 @@
 import {Message} from "@open-wa/wa-automate";
 import {Client} from "../../util/extend/Client";
-import {latestGempa} from "../../util/gempa";
+import {latestGempa} from "../../util/gempa.js";
 export async function run(client:Client, message:Message) {
   const gempa = await latestGempa();
   const teks = `informasi gempa terbaru:\n\nWaktu: *${gempa.tanggal} ${gempa.jam}*\nBujur: *${gempa.bujur}*\nLintang: *${gempa.lintang}*\nMagnitudo: *${gempa.magnitude}*\nKedalaman: *${gempa.kedalaman}*\nLokasi: *${gempa.wilayah}*`.trim();
