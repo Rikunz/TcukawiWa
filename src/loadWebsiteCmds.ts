f8bd9ae3cdcfe2aa5b9a3bd9cc19ac4da7740374
MoonLGH <MoonLMC1212@gmail.com>
2023-02-12 06:02:32 +0000

    fixes cmd.json

--- src/loadWebsiteCmds.ts
+++ src/loadWebsiteCmds.ts
@@ -1,6 +1,7 @@
-import {LoadCommandsNoCollection} from "./util/handle.js";
+import {LoadCommands} from "./util/handle.js";
 import {writeFileSync} from "fs";
-LoadCommandsNoCollection().then((cmds) =>{
+
+LoadCommands().then((cmds) =>{
   cmds.map((cmd) =>{
     cmd.filepath = `src/commands/${cmd.category}/${cmd.filepath}`;
   });
