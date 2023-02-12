a9493a25d23179607f947ccaf4d5f467c861b3c7
ohsyme <49177554+ohsyme@users.noreply.github.com>
2023-02-12 21:05:43 +0700

    source trust me part 2

--- src/commands/downloader/pinterest.ts
+++ src/commands/downloader/pinterest.ts
@@ -4,6 +4,7 @@ import {pinterest} from "../../util/pinterest.js";
 export async function run(client:Client, message:Message) {
   const {args} = client.parseMessage(message);
   const resPint = await pinterest(args[0]);
+  if(resPint ==="ERROR") return client.clientInstances!.sendText(message.chatId, "error");
   client.clientInstances?.sendFileFromUrl(message.chatId, resPint!, "pinterest", "pinterest");
 
 }
