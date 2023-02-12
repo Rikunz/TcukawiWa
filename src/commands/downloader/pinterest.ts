018b700e91fafd0f61924cd28f8506d45ed2ae76
ohsyme <gamingrkp@gmail.com>
2023-02-12 19:35:27 +0700

    pinterest

---
+++ src/commands/downloader/pinterest.ts
@@ -0,0 +1,13 @@
+import {Message} from "@open-wa/wa-automate";
+import {Client} from "../../util/extend/Client";
+import {pinterest} from "../../util/pinterest.js";
+export async function run(client:Client, message:Message) {
+  const {args} = client.parseMessage(message);
+  const resPint = await pinterest(args[0]);
+  client.clientInstances?.sendFileFromUrl(message.chatId, resPint!, "pinterest", "pinterest");
+
+}
+
+export const name = "pinterest";
+export const description = "download pinterest video/photo";
+export const alias = ["pindl"];
---
+++ src/util/pinterest.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+export async function pinterest(url: string){
+    try {
+        const res = await axios.get("https://pinterestvideo.com/", {
+            headers: {
+              "user-agent":
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+            },
+        });
+          
+        const $ = cheerio.load(res.data);
+        const url_name = $("#downloadForm > input").attr("name");
+        const data = {
+            [`${url_name}`]: url,
+        };
+
+        const respon = await axios.post(
+            "https://pinterestvideo.com/downloader.php",
+            new URLSearchParams(Object.entries(data)),
+            {
+              headers: {
+                "user-agent":
+                  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+                  cookie: (res.headers["set-cookie"]?.join(";") as string),
+              },
+            },
+        );
+        
+        const chuaks = cheerio.load(respon.data);
+        return(chuaks("#dlSection > a").attr("href") as string);
+      
+    }catch(error){
+        console.log(error)
+    }
+}
