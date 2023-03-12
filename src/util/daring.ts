2977cbd4c4b896faf11bb5119111573e47d90bca
Rikunz <thoriqfirdaus8@gmail.com>
2023-03-12 13:53:51 +0700

    updet API to get the Data

---
+++ src/util/daring.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export async function daring(command: string) {
+    const response = await axios(`http://api.mautau.ga/${command}`, {
+        method: "GET"
+    });
+    const datasend = response.data;
+    console.log(datasend)
+    return datasend;
+}
