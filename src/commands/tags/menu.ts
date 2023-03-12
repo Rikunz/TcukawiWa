5a4eb7f16fcd1d36e34b90a5babd7ddd408975d0
Rikunz <thoriqfirdaus8@gmail.com>
2023-03-12 13:51:06 +0700

    change help prefix

--- src/commands/tags/menu.ts
+++ src/commands/tags/menu.ts
@@ -1,8 +1,8 @@
-import {Message} from "@open-wa/wa-automate";
-import {Client} from "../../util/extend/Client";
+import { Message } from "@open-wa/wa-automate";
+import { Client } from "../../util/extend/Client";
 
-export async function run(client:Client, message:Message) {
-    client.clientInstances!.sendText(message.chatId, "To see the commands list, please go to https://tcukawi.tech/Tcukawi-WebCommands/commands/");
+export async function run(client: Client, message: Message) {
+    client.clientInstances!.sendText(message.chatId, "Open AI-{Ask/Tanya/Bot/AI} \n What anime is this?-{Wait/whatanimeisthis} \n IG Downloader-{igdl/ig} \n Pinterest-{pindl} \n Tiktok-{ttdl/tikdl} \n Info Gempa-{infogempa} \n In development: \n Youtube Video-{ytdl} \n YT Music-{ytmp3/ytdlmp3}");
 }
 
 export const name = "menu";
